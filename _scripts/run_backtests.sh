d5b35b66a1a1d1befc3ab03fab4abdcd89d63d8a
d-a-t-a-d-i-t <d-a-t-a-d-i-t@users.noreply.github.com>
2015-10-26 16:36:57 +0100

    Test of test.ini

--- _scripts/run_backtests.sh
+++ _scripts/run_backtests.sh
@@ -4,23 +4,19 @@
 set -e
 cd -P -- "$(dirname -- "$0")" && pwd -P
 ROOT="$(git rev-parse --show-toplevel)"
-type run_backtest || run_backtest() { echo run_backtest $*; }
+type run_backtest.sh || run_backtest() { echo run_backtest $*; }
 
 # Find, parse configuration and run the tests.
-find "$ROOT/$1" -type f -name "*.ini" -print0 | while IFS= read -r -d '' file; do
+find "$ROOT/$1" -type f -name "test.ini" -print0 | while IFS= read -r -d '' file; do
   . <(grep = "$file" | sed "s/;/#/g") # Load ini values.
   dir="$(dirname "$file")"
-done
-
-echo 'Cycle starts...'
-for deposit in ${deposits[@]}; do
-  for year in ${years[@]}; do
-    for bt_source in ${bt_sources[@]}; do
-      for spread in ${spreads[@]}; do
-        #run_backtest.sh -f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source -D "$dir" -r "sets/$dir/Report"
-        echo "-f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source -D $dir -r sets/$dir/Report"
+  for deposit in ${deposits[@]}; do
+    for year in ${years[@]}; do
+      for bt_source in ${bt_sources[@]}; do
+        for spread in ${spreads[@]}; do
+          run_backtest.sh -f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source -D "$dir"
+        done
       done
     done
   done
 done
-echo 'Cycle ended!'
