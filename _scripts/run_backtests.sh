df0a080a2396712692aa54a7f951d079983fca66
d-a-t-a-d-i-t <d-a-t-a-d-i-t@users.noreply.github.com>
2015-10-26 17:01:25 +0100

    Test of test.ini II.

--- _scripts/run_backtests.sh
+++ _scripts/run_backtests.sh
@@ -14,6 +14,7 @@ find "$ROOT/$1" -type f -name "test.ini" -print0 | while IFS= read -r -d '' file
     for year in ${years[@]}; do
       for bt_source in ${bt_sources[@]}; do
         for spread in ${spreads[@]}; do
+          echo $dir
           run_backtest.sh -f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source -D "$dir"
         done
       done
