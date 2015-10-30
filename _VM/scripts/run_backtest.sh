b487928ca47b5a820b118e93c11d29a0a8d4ed2c
d-a-t-a-d-i-t <d-a-t-a-d-i-t@users.noreply.github.com>
2015-10-30 14:18:24 +0100

    Suppress Xvfb and Wine messages, dump them on build failure.

--- _VM/scripts/run_backtest.sh
+++ _VM/scripts/run_backtest.sh
@@ -113,4 +113,4 @@ clean_files
 configure_wine
 
 # Run the test in the platform.
-time wine "$TERMINAL_EXE" "config/$CONF" && on_success || on_failure
+time wine "$TERMINAL_EXE" "config/$CONF" &>> ~/wine_terminal.exe.log && on_success || on_failure
